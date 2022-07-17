85038f58de2761cbc50b8c4c57b077734e886360
Tarlan <t-tarlan@yandex.ru>
2022-07-17 22:48:29 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,21 +2,25 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserDao dao = new UserDaoJDBCImpl();
-        dao.createUsersTable();
+        UserService userService = new UserServiceImpl(dao);
+       // d rjycnhernj cthdbcfк в конструктор сервиса
+        userService.createUsersTable();
         System.out.println("Create  table Users");
-        dao.saveUser("Ivan", "Ivanov", (byte) 35);
-        dao.saveUser("Peter", "Petrov", (byte) 20);
-        dao.saveUser("Maria", "Sidorova", (byte) 18);
-        dao.saveUser("Olga", "Kozlova", (byte) 23);
-        dao.getAllUsers();
-        dao.cleanUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 35);
+        userService.saveUser("Peter", "Petrov", (byte) 20);
+        userService.saveUser("Maria", "Sidorova", (byte) 18);
+        userService.saveUser("Olga", "Kozlova", (byte) 23);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
 
-        dao.dropUsersTable();
+        userService.dropUsersTable();
         System.out.println("Drop table Users");
 
     }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,34 +1,57 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    Connection connection;
+    UserDao userDaoJDBCImpl;
+
+    public UserServiceImpl(UserDao userDaoJDBCImpl) {
+        this.userDaoJDBCImpl = userDaoJDBCImpl;
+    }
+
+    public UserServiceImpl() {
+    }
+
+
     public void createUsersTable() {
+
+        userDaoJDBCImpl.createUsersTable();
 
     }
 
     public void dropUsersTable() {
-
+        userDaoJDBCImpl.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDaoJDBCImpl.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id=?");
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("User удален с id " + id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDaoJDBCImpl.getAllUsers();
     }
 
     public void cleanUsersTable() {
+        userDaoJDBCImpl.cleanUsersTable();
 
     }
-
-    UserDaoJDBCImpl userDaoJDBC =  new UserDaoJDBCImpl();
 }
