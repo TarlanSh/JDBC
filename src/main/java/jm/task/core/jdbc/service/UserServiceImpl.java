5cb25d5353305eccbc0980aa7a2bdb8ba46cb940
Tarlan <t-tarlan@yandex.ru>
2022-07-18 00:14:45 +0300

    В классе реализованы методы

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,15 +3,12 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     Connection connection;
-    UserDao userDao =new UserDaoJDBCImpl();
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
@@ -22,9 +19,7 @@ public class UserServiceImpl implements UserService {
 
 
     public void createUsersTable() {
-
         userDao.createUsersTable();
-
     }
 
     public void dropUsersTable() {
@@ -36,15 +31,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public void removeUserById(long id) {
-        PreparedStatement preparedStatement = null;
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id=?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-            System.out.println("User удален с id " + id);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
@@ -53,6 +40,5 @@ public class UserServiceImpl implements UserService {
 
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
-
     }
 }
